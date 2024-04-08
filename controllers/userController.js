528078903163b9b1c60d62dad70cb621374d671b
guren97 <60452104+guren97@users.noreply.github.com>
2024-04-08 22:06:59 +0800

    CREATED protect route for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,12 +5,20 @@ import ErrorResponse from "../utils/errorResponse.js";
 
 import User from "../models/UserSchema.js";
 
+/**
+ * Middleware to register a new user
+ * param {object} req - Express request object
+ * param {object} res - Express response object
+ * param {function} next - Express next function
+ * returns {Promise<void>} - Promise representing the completion of the operation
+ */
 const setUser = asyncHandler(async (req, res, next) => {
+  // Extracting user data from request body
   const { username, first_name, last_name, email, password } = req.body;
 
   try {
+    // Check if user with same username or email already exists
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-    // Check if user exists in database
     if (existingUser) {
       const message =
         existingUser.username === username
@@ -19,7 +27,7 @@ const setUser = asyncHandler(async (req, res, next) => {
       return next(new ErrorResponse(message, 401));
     }
 
-    // create new user
+    // Create new user
     const user = await User.create({
       username,
       first_name,
@@ -29,55 +37,116 @@ const setUser = asyncHandler(async (req, res, next) => {
     });
 
     if (user) {
-      generateToken(user, 201, res); // Call generateToken function here
+      // Generate token for the newly registered user
+      generateToken(user, 201, res);
     }
   } catch (error) {
-    return next(new ErrorResponse("Server error", 500)); // Send error response here
+    // Send server error response
+    return next(new ErrorResponse("Server error", 500));
   }
 });
 
+/**
+ * Middleware to fetch all users
+ * param {object} req - Express request object
+ * param {object} res - Express response object
+ * param {function} next - Express next function
+ * returns {Promise<void>} - Promise representing the completion of the operation
+ */
 const getUsers = asyncHandler(async (req, res, next) => {
   try {
+    // Fetch all users from the database
     const users = await User.find();
-    // if there are no users on database
     if (!users) {
+      // Send error response if no users found
       return next(new ErrorResponse("No users found", 404));
     }
+    // Send success response with users data
     res.status(200).json({
       success: true,
       users,
     });
   } catch (error) {
+    // Send server error response
     next(new ErrorResponse("Server error", 500));
   }
 });
 
-const loginUser = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
-
+/**
+ * Middleware to fetch the current user
+ * param {object} req - Express request object
+ * param {object} res - Express response object
+ * param {function} next - Express next function
+ * returns {Promise<void>} - Promise representing the completion of the operation
+ */
+const getCurrentUser = asyncHandler(async (req, res, next) => {
   try {
-    const user = await User.findOne({ email }).select("+password");
+    // Retrieve the user ID from the decoded JWT token
+    const userId = req.user.id;
 
+    // Find the user by ID and exclude the password field
+    const user = await User.findById(userId).select("-password");
+
+    // Check if the user exists
     if (!user) {
       return next(new ErrorResponse("User not found", 404));
     }
 
+    // Send the user data in the response
+    res.status(200).json({ user });
+  } catch (error) {
+    // Handle server errors
+    return next(new ErrorResponse("Server Error", 500));
+  }
+});
+
+/**
+ * Middleware to authenticate user login
+ * param {object} req - Express request object
+ * param {object} res - Express response object
+ * param {function} next - Express next function
+ * returns {Promise<void>} - Promise representing the completion of the operation
+ */
+const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email and select password field
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      // Send error response if user not found
+      return next(new ErrorResponse("User not found", 404));
+    }
+
+    // Compare passwords
     const matchedPassword = await bcrypt.compare(password, user.password);
 
     if (!matchedPassword) {
+      // Send error response if password is incorrect
       return next(new ErrorResponse("Wrong password", 401));
     }
 
     if (user) {
-      generateToken(user, 201, res); // Call generateToken function here
+      // Generate token for the authenticated user
+      generateToken(user, 201, res);
     }
   } catch (error) {
     console.error("Error in setUser:", error);
-    return next(new ErrorResponse("Server error", 500)); // Use return statement to exit the function
+    // Send server error response
+    return next(new ErrorResponse("Server error", 500));
   }
 });
 
+/**
+ * Middleware to logout user
+ * param {object} req - Express request object
+ * param {object} res - Express response object
+ * param {function} next - Express next function
+ * returns {void} - Void function
+ */
 const logoutUser = asyncHandler(async (req, res, next) => {
+  // Clear JWT cookie and send success message
   res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
@@ -85,4 +154,4 @@ const logoutUser = asyncHandler(async (req, res, next) => {
   res.status(200).json({ message: "User logged out" });
 });
 
-export { setUser, getUsers, loginUser, logoutUser };
+export { setUser, getUsers, getCurrentUser, loginUser, logoutUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const userRouter = express.Router();
+
 import protect from "../middleware/authMiddleware.js";
 
 import {
+  getCurrentUser,
   getUsers,
   loginUser,
   logoutUser,
@@ -14,10 +16,6 @@ userRouter.route("/").get(protect, getUsers);
 userRouter.route("/login").post(loginUser);
 userRouter.route("/logout").post(logoutUser);
 
-// userRouter.route("/login").post(loginUser);
-// userRouter.route("/logout").post(logoutUser);
-
-// ## PROTECT - Protect route using auth middleware
-// userRouter.route("/current").get(getCurrentUser);
+userRouter.route("/current").get(protect, getCurrentUser);
 
 export default userRouter;
